26566d3e68989da9084ba415ac794c2350da102e
webstrap <kong83@gmail.com>
2023-07-14 13:48:38 +0200

    Remove direct ioRedis import + switch to pnpm

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
 import {VercelRequest, VercelResponse} from "@vercel/node";
 import Bull from 'bull';
 import {Queue} from "bullmq";
-import IORedis from 'ioredis';
+
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379");
+const REDIS_USERNAME = process.env.REDIS_USERNAME || '';
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || '';
 
 export default async function handleRequest(req: VercelRequest, res: VercelResponse): Promise<void> {
 
@@ -11,16 +16,21 @@ export default async function handleRequest(req: VercelRequest, res: VercelRespo
     }
 
     if (req.method === 'GET') {
-        const REDIS_URL = process.env.REDIS_URL || '';
         new Bull('vercel-bull', REDIS_URL);
-        if(!REDIS_URL) {
+        if (!REDIS_URL || !REDIS_HOST || !REDIS_PORT || !REDIS_USERNAME || !REDIS_PASSWORD) {
             res.status(400).send({success: false, message: 'Redis Configuration missing'}).end();
             return;
         }
-        const connection = new IORedis(REDIS_URL);
-        const queue = new Queue('vercel-example-queue', {connection});
+        const queue = new Queue('vercel-example-queue', {
+            connection: {
+                host: REDIS_HOST,
+                port: REDIS_PORT,
+                username: REDIS_USERNAME,
+                password: REDIS_PASSWORD,
+            }
+        });
 
-        const job = await queue.add('example-job', { foo: 'bar' });
+        const job = await queue.add('example-job', {foo: 'bar'});
 
         res.status(200).send({success: job?.id}).end();
     }
